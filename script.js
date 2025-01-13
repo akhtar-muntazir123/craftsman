0d4c8a8c9689f71a14f8b21fc4676f754b567e7a
muntazir <muntazirakhtar21@gmail.com>
2025-01-13 11:05:47 +0530

    responsive done

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//toggle function for responsive navbar
+
+function toggleMenu() {
+    const navLinks = document.querySelector('#nav-links');
+    navLinks.classList.toggle('show');
+    console.log("hey")
+  }
+  
